63ba807e8d303a125ff690b22148047bde314531
krsarmiento <krsarmiento@gmail.com>
2014-02-14 09:01:38 -0500

    Improved movie loader.

---
+++ assets/js/functions.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    
+    $('.loadMovies').on('click', function() {
+       var button = $(this);
+       var id = this.id.split(',');
+       var rating = id[0];
+       var times = id[1];
+       
+       $.ajax({
+           url: 'ajax/movies/load/' + rating + '/' + times,
+       }).done(function(movies) {
+           if (movies) {
+                button.before(movies);
+                button.attr('id', rating+','+(parseInt(times)+1));
+           } else {
+               button.attr('disabled', true);
+           }
+       });
+       
+    });
+    
+    
+});
